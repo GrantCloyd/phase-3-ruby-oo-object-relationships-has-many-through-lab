de6d77d75e5ba196cedac0d7410288fc8034c75a
Grant Cloyd <dgcloyd@gmail.com>
2021-07-01 14:14:49 -0500

    commit tree ce917dff7a5134a1ee201778d894859fb93f5b62

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,17 @@
+class Appointment 
+attr_reader :doctor, :patient, :date
 
+  @@all = []
+  def self.all 
+    @@all
+  end
+
+def initialize(date, patient, doctor)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+    end
+
+
+end
